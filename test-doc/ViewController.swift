934c8e3a91e1443dec6fb09af6e63a80933de553
Artem <omichka@protonmail.com>
2025-07-14 17:54:22 +0300

    Initial Commit

---
+++ test-doc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test-doc
+//
+//  Created by Artem Golovanev on 14.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
